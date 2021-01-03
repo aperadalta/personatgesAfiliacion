8ff8e8fb79e8ee100db396c33e3e7f41d5614ec6
Elheyr <adria.peradalta@gmail.com>
2021-01-03 22:43:44 +0100

    few updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var win = $(window),
-    sectionHeight = 1000,
+    sectionHeight = 1100,
     currentSection = 0,
     lastSection = 0;
 
@@ -7,48 +7,59 @@ function changeCircles(n){
     let child = `.pjs:nth-child(${n})`;
 
     $('.pjs').css({
-        "height": "30px",
-        "width": "30px",
-        "margin-left": "-18px",
-        "border": "0px black solid"
+        "height": "40px",
+        "width": "40px",
+        "margin-left": "-23px",
+        "border": "2px grey solid",
+        "box-shadow": "none"
     })
 
     $(child).css({
-        "height": "36px",
-        "width": "36px",
-        "margin-left": "-21px",
-        "border": "3px black solid"
+        "height": "46px",
+        "width": "46px",
+        "margin-left": "-26px",
+        "border": "3px black solid",
+        "box-shadow": "0px 0px 10px 5px rgb(102, 186, 255)"
     })
 }
 
 function disappear(n, n2){
     let child = `.full:nth-child(${n2})`;
 
+    
+
     if(n > n2){
         $(child).css({
             "opacity": "0",
             "transform": "scale(2)"
         })
+        $('#titol').css("opacity", "0");
     } else {
         $(child).css({
             "opacity": "0",
             "transform": "scale(0.1)"
         })
+        if(n == 0) $('#titol').css("opacity", "1");
     }
 }
 
 function appear(n){
     let child = `.full:nth-child(${n})`;
-
     $(child).css({
         "opacity": "1",
         "transform": "scale(1)"
     })
 }
 
-function myFunction(n, n2) {
-    console.log(n +" "+n2);
+function fullClear(){
+    $('.full').css({
+        "opacity": "0"
+    })
 }
+
+ function myFunction(n, n2) {
+    console.log(n +" "+n2);
+} 
 
 win.scroll(function (e) {
     var calculateSection = parseInt(win.scrollTop() / sectionHeight, 10);
@@ -56,7 +67,7 @@ win.scroll(function (e) {
         lastSection = currentSection
         currentSection = calculateSection;
 
-       // myFunction(currentSection, lastSection);
+       myFunction(currentSection, lastSection);
         
        changeCircles(currentSection);
        appear(currentSection); 
@@ -65,9 +76,17 @@ win.scroll(function (e) {
 });
 
 
+/* 
+function fullClear(){
+    $('.full').css({
+        "opacity": "0"
+    })
+}
+
 $('.pjs').click((e)=>{
     var index = e.target.id;
-    
+
+    fullClear();
     appear(index);
     changeCircles(index);
-})
+}) */
